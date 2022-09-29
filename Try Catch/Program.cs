7d5146b9259c1477e13badb6b04f64cf4c7e9908
nmepani87 <108291876+nmepani87@users.noreply.github.com>
2022-09-29 01:48:32 +0100

    Exception

---
+++ Try Catch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Try_Catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Creation of list with numbers
+                List<int> numlist = new List<int> { 26, 10, 50, 32 };
+                Console.WriteLine("Please enter a number for other numbers to be divided by:");
+                // Converting to an int what the user enters 
+                int user_number = Convert.ToInt32(Console.ReadLine());
+                // Iteration through list
+                foreach (var num in numlist)
+                {
+                    // Prints results of each variable in list divided by user input
+                    Console.WriteLine(num / user_number);
+                }
+            }
+            // no exception errors given with dividing with negative number
+            // catch triggered when user inputs 0
+            catch (DivideByZeroException)
+            { 
+                Console.WriteLine("Please type in a number greater than 0"); 
+            }
+            // catch triggered when user inputs a string
+            catch (FormatException)
+            {
+                Console.WriteLine("Please type in a number/just a number, not letters");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
