7e1c27ba666b404a4314f1ba366f9d355fa8d4e2
nmepani87 <108291876+nmepani87@users.noreply.github.com>
2022-09-29 17:28:54 +0100

    Method assignment

---
+++ Methods/Conversion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Conversion
+    {
+        // Creating a method and giving it a parameter (userinput for main program)
+        public int Fahrenheit(int userinput)
+        {
+            // Converting the result of the sum to an integer and assigning to a int variable name
+            int CtoF = (Convert.ToInt32(userinput * 9 / 5) + 32);
+            // Returning variable so can be used outside class
+            return CtoF;
+        }
+
+        // Creating a method and giving it a parameter (userinput for main program)
+        public int Kelvin(int userinput)
+        {
+            // Converting the result of the sum to an integer and assigning to a int variable name
+            int CtoK = (Convert.ToInt32(userinput + 273.15));
+            // Returning variable so can be used outside class
+            return CtoK;
+        }
+
+        // Creating a method and giving it a parameter (userinput for main program)
+        public int Rankine(int userinput)
+        {
+            // Converting the result of the sum to an integer and assigning to a int variable name
+            int RtoK = (Convert.ToInt32(userinput * 9 / 5) + (Convert.ToInt32(491.67)));
+            // Returning variable so can be used outside class
+            return RtoK;
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating new object and assigning to variable
+            Conversion conversion = new Conversion();
+            // Printing string into console that asks user question
+            Console.WriteLine("Please enter what the current temperature is in the Celsius scale:");
+            // Reading what the inputs into the console, converting that into an integer and storing it to an integer variable
+            int userinput = Convert.ToInt32(Console.ReadLine());
+            // Giving a certain method within the object the int value to process and storing end result to int variable
+            int resultFtemp = conversion.Fahrenheit(userinput);
+            // Writing a combination of string and int variables into console
+            Console.WriteLine(userinput + "°C converted into Fahrenheit's is " + resultFtemp + "°F");
+            // Giving a certain method within the object the int value to process and storing end result to int variable
+            int resultKtemp = conversion.Kelvin(userinput);
+            // Writing a combination of string and int variables into console
+            Console.WriteLine(userinput + "°C converted into Kelvin's is " + resultKtemp + " K");
+            // Giving a certain method within the object the int value to process and storing end result to int variable
+            int resultRtemp = conversion.Rankine(userinput);
+            // Writing a combination of string and int variables into console
+            Console.WriteLine(userinput + "°C converted into Rankine's is " + resultRtemp + "°R");
+        }
+    }
+}
