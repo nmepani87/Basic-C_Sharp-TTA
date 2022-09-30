43f01638136933f551041e4b7ebec6c1d99e5197
nmepani87 <108291876+nmepani87@users.noreply.github.com>
2022-10-01 00:06:12 +0100

    Inheritance

---
+++ Inheritance/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Person
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        internal void SayName()
+        {
+            Console.WriteLine("Name: [" + FirstName + " " + LastName +"]");
+            return;
+
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating object and giving values to properties
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student"};
+
+            // Running method within the superclass of the objects class
+            employee.SayName();
+        }
+    }
+}
