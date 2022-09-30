09e7510d69d4b1aaf128077fa210893826b837e1
nmepani87 <108291876+nmepani87@users.noreply.github.com>
2022-09-30 02:36:52 +0100

    Void Method

---
+++ Void Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating new object and assigning to variable
+            Temperature temperature = new Temperature();
+            // Giving values to two integer variables
+            int intinput1 = 66;
+            int intinput2 = 13;
+            // Passing these variables into the method
+            temperature.AverageTemp(intinput1, intinput2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Void Method/Temperature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    internal class Temperature
+    {
+        // Void method so no value is returned ( two parameters given)
+        public void AverageTemp(int intinput1, int intinput2)
+        {
+            // sum that uses one of the parameters passed through, but value not returned after
+            int total = intinput1 / 2;
+            // writing to console the value of the other parameter 
+            Console.WriteLine(intinput2);
+        }
+    }
+}
