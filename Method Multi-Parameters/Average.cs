697dee3b3d7b8f1b7f0cb4dfd8be6afe3b148a6f
nmepani87 <108291876+nmepani87@users.noreply.github.com>
2022-09-30 01:11:14 +0100

    Method Assignment

---
+++ Method Multi-Parameters/Average.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Multi_Parameters
+{
+    internal class Average
+    {
+        // Adding 1 fixed parameter to pass through plus setting
+        // creating 2nd optional parameter with default value
+        public int AverageTemp(int userinput1, int userinput2 = 0)
+        {
+            // Doing a sum with the values giving the returning result to outside method
+            int averageresult = (userinput1 + userinput2) / 2 ;
+            return averageresult;
+        }
+    }
+}
---
+++ Method Multi-Parameters/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Multi_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating new object and assigning to variable
+            Average average = new Average();
+            // Printing string into console that asks user question
+            Console.WriteLine("Please enter what the maximum temperature will be today, if unknown enter what you think it will reach");
+            // Reading what the inputs into the console, converting that into an integer and storing it to an integer variable
+            int userinput1 = Convert.ToInt32(Console.ReadLine());
+            // Printing string into console that asks user question
+            Console.WriteLine("Please enter what the minimum temperature will be today, if unsure can you leave it blank");
+            // Reading what the inputs into the console and storing it to an string variable
+            string userinput2 = Console.ReadLine();
+
+            // If users secound input is blank then if block executed
+            if (userinput2 == "")
+            {
+                // Writing a combination of string and int variables into console, plus gives parameters to the method
+                Console.WriteLine("The average temperature for today will be " + average.AverageTemp(userinput1) + "°F");
+            }
+            else
+            {
+                // If user input isn't blank, input its converted to integer and assigned to a variable
+                int userinput2int = Convert.ToInt32(userinput2);
+                // Writing a combination of string and int variables into console, plus giving both parameters to the method
+                Console.WriteLine("The average temperature for today will be " + average.AverageTemp(userinput1, userinput2int) + "°F");
+            }
+        }
+    }
+}
