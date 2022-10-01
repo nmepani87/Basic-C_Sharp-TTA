db8d90fbb27666cd55e9403e11a031c59a24f2a1
nmepani87 <108291876+nmepani87@users.noreply.github.com>
2022-10-01 23:52:04 +0100

    Abstract Class submission

---
+++ Abstract/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    internal class Employee : Person
+    {
+        // Overridding method thats in the superclass
+        public override void SayName()
+        {
+            // Printing object value and strings to console 
+            Console.WriteLine("Name: [" + FirstName + " " + LastName + "]");
+            return;
+        }
+    }
+}
