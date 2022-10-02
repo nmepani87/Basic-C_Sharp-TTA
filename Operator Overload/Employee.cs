f1cecc5d0e7aa76af37306811452154ad0e9a25b
nmepani87 <108291876+nmepani87@users.noreply.github.com>
2022-10-02 22:50:33 +0100

    Operator Overload

---
+++ Operator Overload/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overload
+{
+    internal class Employee
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int ID { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public static bool operator ==(Employee EmployeeA, Employee EmployeeB)
+        {
+            if (EmployeeA.ID.Equals(EmployeeB.ID))
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Employee EmployeeA, Employee EmployeeB)
+        {
+            if (EmployeeA.ID.Equals(EmployeeB.ID))
+                return false;
+            else
+                return true;
+        }
+    }
+}
---
+++ Operator Overload/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating object and giving values to each property within
+            Employee employeeA = new Employee
+            {
+                FirstName = "John",
+                LastName = "Smith",
+                ID = 1
+            };
+            // Creating object and giving values to each property within, but leaving ID value same as first objects
+            Employee employeeB = new Employee
+            {
+                FirstName = "Henry",
+                LastName = "Smyth",
+                ID = 1
+            };
+
+            // Running comparision check and printing to console instead of true/false a string dependant on result
+            Console.WriteLine(employeeA == employeeB ? "These two people have the same ID" : 
+                "The two people being compared don't have the same ID");
+
+
+
+
+        }
+    }
+}
